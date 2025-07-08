6f131d1b875c0a23d0fd42a20090a35237236cd9
venkatKrishnan86 <88517272+venkatKrishnan86@users.noreply.github.com>
2025-07-08 19:33:17 -0400

    Changed the function to a class for ease of use

--- augmentation.py
+++ augmentation.py
@@ -20,44 +20,53 @@ def phase_intercept_distortion(audio: np.ndarray, theta: float):
         "The audio array must be atleast 1 dimensional of the form (*, samples)"
     return np.real(hilbert(audio, axis=-1) * np.exp(1j*theta))
 
-def apply_augmentation_phase_intercept_distortion(
-        audio: torch.Tensor, 
-        theta: torch.Tensor = None
-    ):
+class PhaseInterceptDistortion(torch.nn.Module):
     """
-        Arguments:
-        -----------
-        audio: torch.Tensor
-            A 2D tensor representing the audio signal with shape (channels, samples).
-
-        theta: torch.Tensor
-            A 0D tensor or a 1D tensor of phase intercept distortion angles in radians.
-            If 1D, it should have shape (batch_size,).
-            If None, defaults to random angles in the range [-pi, pi] with a shape of (batch_size,).
-            The phase intercept distortion angle in radians.
+    A PyTorch module to apply phase intercept distortion to audio signals by applying 
+    a frequency-independent phase shift to the input audio tensor.
     """
+    def __init__(self):
+        super(PhaseInterceptDistortion, self).__init__()
 
-    assert len(audio.shape) >= 2, \
-        "The audio tensor must be 2 dimensional of the form (channels, samples) or 3 dimensional of the form (batch_size, channels, samples)"
-    assert len(theta.shape) == 1 and theta.shape[0] == audio.shape[0], \
-        "The theta tensor must be 1 dimensional of the form (batch_size,)"
-    
-    if theta is None:
-        theta = torch.rand(audio.shape[0]) * 2 * np.pi - np.pi
+    def forward(
+            self, 
+            audio: torch.Tensor, 
+            theta: torch.Tensor = None
+        ):
+        """
+            Arguments:
+            -----------
+            audio: torch.Tensor
+                A 2D tensor representing the audio signal with shape (channels, samples).
 
-    X = torch.fft.fft(audio, dim=-1, norm='ortho')
-    num_samples = X.shape[-1]
-    theta = theta.to(audio.device).unsqueeze(-1)
+            theta: torch.Tensor
+                A 0D tensor or a 1D tensor of phase intercept distortion angles in radians.
+                If 1D, it should have shape (batch_size,).
+                If None, defaults to random angles in the range [-pi, pi] with a shape of (batch_size,).
+                The phase intercept distortion angle in radians.
+        """
 
-    if len(X.shape) == 2:
-        X[:, :num_samples//2] *= torch.exp(theta * torch.tensor(1j).to(audio.device))
-        X[:, num_samples//2:] *= torch.exp(-theta * torch.tensor(1j).to(audio.device))
-    elif len(X.shape) == 3:
-        X[:, :, :num_samples//2] *= torch.exp(theta * torch.tensor(1j).to(audio.device)).unsqueeze(1)
-        X[:, :, num_samples//2:] *= torch.exp(-theta * torch.tensor(1j).to(audio.device)).unsqueeze(1)
-    else:
-        raise ValueError("The audio tensor must be 2 or 3 dimensional.")
-    return torch.fft.ifft(X, dim=-1, norm='ortho').real
+        assert len(audio.shape) >= 2, \
+            "The audio tensor must be 2 dimensional of the form (channels, samples) or 3 dimensional of the form (batch_size, channels, samples)"
+        assert len(theta.shape) == 1 and theta.shape[0] == audio.shape[0], \
+            "The theta tensor must be 1 dimensional of the form (batch_size,)"
+        
+        if theta is None:
+            theta = torch.rand(audio.shape[0]) * 2 * np.pi - np.pi
+
+        X = torch.fft.fft(audio, dim=-1, norm='ortho')
+        num_samples = X.shape[-1]
+        theta = theta.to(audio.device).unsqueeze(-1)
+
+        if len(X.shape) == 2:
+            X[:, :num_samples//2] *= torch.exp(theta * torch.tensor(1j).to(audio.device))
+            X[:, num_samples//2:] *= torch.exp(-theta * torch.tensor(1j).to(audio.device))
+        elif len(X.shape) == 3:
+            X[:, :, :num_samples//2] *= torch.exp(theta * torch.tensor(1j).to(audio.device)).unsqueeze(1)
+            X[:, :, num_samples//2:] *= torch.exp(-theta * torch.tensor(1j).to(audio.device)).unsqueeze(1)
+        else:
+            raise ValueError("The audio tensor must be 2 or 3 dimensional.")
+        return torch.fft.ifft(X, dim=-1, norm='ortho').real
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="Apply phase intercept distortion to a single audio.")
